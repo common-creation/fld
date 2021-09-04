b681af9a8046680f0f297828dbb00cd002ce65eb
mohemohe <mohemohe@ghippos.net>
2021-09-04 23:59:17 +0900

    辞書に文言追加 close #2

--- i18n/ja.go
+++ i18n/ja.go
@@ -20,5 +20,38 @@ var ja = Dictionary{
 			"build":  "ビルド結果",
 			"deploy": "デプロイ結果",
 		},
+		"admin-web": Dictionary{
+			"usage": "Web管理画面 をデプロイ対象に加えます",
+		},
+		"all": Dictionary{
+			"usage": "全てのモジュールをデプロイ対象に加えます",
+		},
+		"bi": Dictionary{
+			"usage": "統計モジュール をデプロイ対象に加えます",
+		},
+		"bosai": Dictionary{
+			"usage": "防災モジュール をデプロイ対象に加えます",
+		},
+		"distribution": Dictionary{
+			"usage": "配信モジュール をデプロイ対象に加えます",
+		},
+		"liff": Dictionary{
+			"usage": "LIFFモジュール をデプロイ対象に加えます",
+		},
+		"platform": Dictionary{
+			"usage": "汎用API をデプロイ対象に加えます",
+		},
+		"scenario": Dictionary{
+			"usage": "シナリオモジュール をデプロイ対象に加えます",
+		},
+		"sequential": Dictionary{
+			"usage": "モジュールのビルドを逐次で行います",
+		},
+		"survey": Dictionary{
+			"usage": "帳票モジュール をデプロイ対象に加えます",
+		},
+		"useContainer": Dictionary{
+			"usage": "ビルドにDockerを使用します",
+		},
 	},
 }
